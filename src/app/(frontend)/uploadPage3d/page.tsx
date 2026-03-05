baea7ecdbf369c417f54952198a847760d4bb090
deki84 <deki1984@hotmail.de>
2026-03-05 12:16:10 +0100

    feat: add auth protection to  uploadPage3d

--- src/app/(frontend)/uploadPage3d/page.tsx
+++ src/app/(frontend)/uploadPage3d/page.tsx
@@ -1,5 +1,9 @@
 import ScenesView from '@/app/components/ScenesView'
 import Sidebar from '@/app/components/Sidebar'
+import { headers as getHeaders } from 'next/headers.js'
+import { getPayload } from 'payload'
+import { redirect } from 'next/navigation'
+import config from '@/payload.config'
 
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
@@ -21,6 +25,12 @@ async function getScenes(): Promise<Scene[]> {
 }
 
 export default async function uploadPage3d() {
+  // Auth-Check
+  const headers = await getHeaders()
+  const payload = await getPayload({ config: await config })
+  const { user } = await payload.auth({ headers })
+  if (!user) redirect('/login')
+
   const scenes = await getScenes()
 
   return (
